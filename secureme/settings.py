1506f59f2216c758b591768e4baa745f0e400bad
Bhushan Wanjari <bhushanwanjari21z@gmail.com>
2022-10-07 20:34:32 +0530

    smtp gone

--- secureme/settings.py
+++ secureme/settings.py
@@ -130,7 +130,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'securemeotp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Secureme@'
+EMAIL_HOST_PASSWORD = 'zhdvzbezmyqsfstl'
 
 
 import django_on_heroku
